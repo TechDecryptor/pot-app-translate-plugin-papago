73c9188ffc8d1d682fa281e1f3065cc16cb77625
TechDecryptor <145176897+TechDecryptor@users.noreply.github.com>
2023-10-23 16:36:11 +0000

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,53 @@
 use serde_json::Value;
 use std::collections::HashMap;
 use std::error::Error;
-use urlencoding::encode;
+
+const URL: &str = "https://papago.naver.com/apis/n2mt/translate";
 
 #[no_mangle]
 pub fn translate(
-    text: &str, // 待翻译文本
-    from: &str, // 源语言
-    to: &str,   // 目标语言
-    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
-    detect: &str, // 检测到的语言 (若使用 detect, 需要手动转换)
-    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+    text: &str,
+    from: &str,
+    to: &str,
+    _detect: &str,
+    _needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
 
-    let mut url = match needs.get("requestPath") {
-        Some(url) => url.to_string(),
-        None => "lingva.pot-app.com".to_string(),
-    };
-    if url.is_empty() {
-        url = "lingva.pot-app.com".to_string();
-    }
-    if !url.starts_with("http") {
-        url = format!("https://{}", url);
-    }
+    let (hash, uuid, time) = get_token()?;
 
-    let plain_text = text.replace("/", "@@");
-    let encode_text = encode(plain_text.as_str());
+    let mut form_data = HashMap::new();
+    form_data.insert("deviceId", uuid.as_str());
+    form_data.insert("locale", to);
+    form_data.insert("dict", "false");
+    form_data.insert("dictDisplay", "30");
+    form_data.insert("honorific", "false");
+    form_data.insert("instant", "false");
+    form_data.insert("paging", "false");
+    form_data.insert("source", from);
+    form_data.insert("target", to);
+    form_data.insert("text", text);
 
-    let res: Value = client
-        .get(format!("{url}/api/v1/{from}/{to}/{encode_text}"))
+    let res = client
+        .post(URL)
+        .header("Authorization", format!("PPG {}:{}", uuid, hash))
+        .header(
+            "Content-Type",
+            "application/x-www-form-urlencoded; charset=UTF-8",
+        )
+        .header("Timestamp", time)
+        .form(&form_data)
         .send()?
         .json()?;
 
     fn parse_result(res: Value) -> Option<String> {
-        let result = res.as_object()?.get("translation")?.as_str()?.to_string();
+        let result = res
+            .as_object()?
+            .get("translatedText")?
+            .as_str()?
+            .to_string();
 
-        Some(result.replace("@@", "/"))
+        Some(result)
     }
     if let Some(result) = parse_result(res) {
         return Ok(Value::String(result));
@@ -45,14 +56,46 @@ pub fn translate(
     }
 }
 
+fn get_version() -> Result<String, Box<dyn Error>> {
+    use regex::Regex;
+    let main_url = "https://papago.naver.com";
+    let main_re = Regex::new(r#"\/main.([^"]+)"#)?;
+    let script_re = Regex::new(r#"v1.([^"]+)"#)?;
+    let client = reqwest::blocking::ClientBuilder::new().build()?;
+    let data = client.get(main_url).send()?.text()?;
+    let script_url = format!("{main_url}{}", main_re.find(&data).unwrap().as_str());
+    let script = client.get(script_url).send()?.text()?;
+    let version = script_re.find(&script).unwrap().as_str();
+    return Ok(version.to_string());
+}
+
+fn get_token() -> Result<(String, String, i64), Box<dyn Error>> {
+    use base64::{engine::general_purpose, Engine as _};
+    use chrono::prelude::*;
+    use hmac::{Hmac, Mac};
+    use md5::Md5;
+    use uuid::Uuid;
+
+    let uuid = Uuid::new_v4().to_string();
+    let time = Local::now().timestamp_millis();
+    let version = get_version()?;
+    let key = version.as_bytes();
+    let data = format!("{uuid}\n{URL}\n{time}");
+    let data = data.as_bytes();
+    type HmacMd5 = Hmac<Md5>;
+    let mut mac = HmacMd5::new_from_slice(key)?;
+    mac.update(data);
+    let hash = general_purpose::STANDARD.encode(mac.finalize().into_bytes());
+    return Ok((hash, uuid, time));
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn try_request() {
-        let mut needs = HashMap::new();
-        needs.insert("requestPath".to_string(), "lingva.pot-app.com".to_string());
-        let result = translate("你好 世界！", "auto", "en", "zh_cn", needs).unwrap();
-        println!("{result}");
+        let needs = HashMap::new();
+        let result = translate("你好 世界！", "auto", "pt", "", needs);
+        println!("{result:?}");
     }
 }
